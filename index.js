3e114000175eea102617a4b3d33b1189c846d84a
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-11-25 21:40:24 +0600

    chore(server): setup express server with mongodb connection and basic products route

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express()
+const cors = require('cors');
+const port = process.env.PORT || 5000
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+//Middleware
+app.use(cors())
+app.use(express.json())
+
+// Database 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xue6gdd.mongodb.net/?appName=Cluster0`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // await client.connect();
+        //MongoDB
+        const db = client.db("nextShopDB");
+        const productsCollection = db.collection('products')
+
+        // All products
+        app.get('/all-products', async (req, res) => {
+            const result = await productsCollection.find().toArray();
+            res.send(result);
+        });
+
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res)=> {
+    res.send('NextShop Server is Running...')
+})
+
+app.listen(port, ()=> {
+    console.log(`Server running on port: ${port}`);
+})
